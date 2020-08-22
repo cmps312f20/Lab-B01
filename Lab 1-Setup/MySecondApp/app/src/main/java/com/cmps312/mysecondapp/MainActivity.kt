30aa945535a077fca70113f0274b5ea60928a5b8
abdalabaaji <abdala.baaji@gmail.com>
2020-08-22 06:09:32 +0300

    update

--- Lab 1-Setup/MySecondApp/app/src/main/java/com/cmps312/mysecondapp/MainActivity.kt
+++ Lab 1-Setup/MySecondApp/app/src/main/java/com/cmps312/mysecondapp/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        showbtn.setOnClickListener {
+        showBtn.setOnClickListener {
             val name = nameEdt.text.toString()
             displayTv.text = "Welcome to CMPS 312 Mr . $name"
         }
