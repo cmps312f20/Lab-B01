c2559e7088cc249af0416854e82f885b8d5728da
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-12 00:12:18 +0300

    update

--- Lab 4 - Android UI 1/Tutorial/app/src/main/java/com/example/androidui/MainActivity.kt
+++ Lab 4 - Android UI 1/Tutorial/app/src/main/java/com/example/androidui/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.androidui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.stadium_item)
+        setContentView(R.layout.login)
 
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
     }
+
 }
+ 
