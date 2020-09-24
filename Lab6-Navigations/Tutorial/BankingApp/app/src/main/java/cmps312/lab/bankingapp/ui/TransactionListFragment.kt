1b571d3b214d1c8804b41f8bd8b64b33faf71757
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-24 16:37:48 +0300

    updated

--- Lab6-Navigations/Tutorial/BankingApp/app/src/main/java/cmps312/lab/bankingapp/ui/TransactionListFragment.kt
+++ Lab6-Navigations/Tutorial/BankingApp/app/src/main/java/cmps312/lab/bankingapp/ui/TransactionListFragment.kt
@@ -3,11 +3,23 @@ package cmps312.lab.bankingapp.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import cmps312.lab.bankingapp.R
+import kotlinx.android.synthetic.main.fragment_transaction_list.*
 
 class TransactionListFragment : Fragment(R.layout.fragment_transaction_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        fundTransferBtn.setOnClickListener {
+
+        }
+        transDetailsBtn.setOnClickListener {
+            val action = R.id.action_transactionListFragment_to_transactionDetailsFragment
+            findNavController().navigate(action)
+        }
+
     }
 }
