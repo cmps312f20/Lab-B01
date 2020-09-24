d4099e4e53dab7700894c1d10c64eb055b2dc8fd
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-24 16:45:38 +0300

    updated

--- Lab6-Navigations/Tutorial/BankingApp/app/src/main/java/cmps312/lab/bankingapp/ui/TransactionListFragment.kt
+++ Lab6-Navigations/Tutorial/BankingApp/app/src/main/java/cmps312/lab/bankingapp/ui/TransactionListFragment.kt
@@ -14,12 +14,15 @@ class TransactionListFragment : Fragment(R.layout.fragment_transaction_list) {
         super.onViewCreated(view, savedInstanceState)
 
         fundTransferBtn.setOnClickListener {
-
+            val action = R.id.action_transactionListFragment_to_transactionDetailsFragment
+            findNavController().navigate(R.id.action_transactionListFragment_to_transferFragment)
         }
         transDetailsBtn.setOnClickListener {
             val action = R.id.action_transactionListFragment_to_transactionDetailsFragment
             findNavController().navigate(action)
         }
+
+        activity?.onBackPressed()
 
     }
 }
